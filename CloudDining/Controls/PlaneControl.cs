7f718758ddc76ffc4969db9662eac1c070a709db
lyrvnrtn <hoge@hoge.com>
2012-11-30 10:56:57 +0900

    PlaneControlを作成
    紙飛行機用。PlaneStatusで向き変更

---
+++ CloudDining/Controls/PlaneControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CloudDining.Controls
+{
+    /// <summary>
+    /// このカスタム コントロールを XAML ファイルで使用するには、手順 1a または 1b の後、手順 2 に従います。
+    ///
+    /// 手順 1a) 現在のプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CloudDining.Controls"
+    ///
+    ///
+    /// 手順 1b) 異なるプロジェクトに存在する XAML ファイルでこのカスタム コントロールを使用する場合
+    /// この XmlNamespace 属性を使用場所であるマークアップ ファイルのルート要素に
+    /// 追加します:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:CloudDining.Controls;assembly=CloudDining.Controls"
+    ///
+    /// また、XAML ファイルのあるプロジェクトからこのプロジェクトへのプロジェクト参照を追加し、
+    /// リビルドして、コンパイル エラーを防ぐ必要があります:
+    ///
+    ///     ソリューション エクスプローラーで対象のプロジェクトを右クリックし、
+    ///     [参照の追加] の [プロジェクト] を選択してから、このプロジェクトを参照し、選択します。
+    ///
+    ///
+    /// 手順 2)
+    /// コントロールを XAML ファイルで使用します。
+    ///
+    ///     <MyNamespace:PlaneControl/>
+    ///
+    /// </summary>
+    public class PlaneControl : Control
+    {
+        static PlaneControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(PlaneControl), new FrameworkPropertyMetadata(typeof(PlaneControl)));
+        }
+
+        public PlaneStateType PlaneStatus
+        {
+            get { return (PlaneStateType)GetValue(PlaneStatusProperty); }
+            set { SetValue(PlaneStatusProperty, value); }
+        }
+        public static readonly DependencyProperty PlaneStatusProperty = DependencyProperty.Register(
+            "PlaneStatus", typeof(PlaneStateType), typeof(PlaneControl), new UIPropertyMetadata(PlaneStateType.Left));
+    }
+    public enum PlaneStateType
+    { Left, Right, }
+    public class PlaneTypeIdToUriConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == DependencyProperty.UnsetValue)
+                return null;
+
+            var url = new Uri(
+                string.Format("pack://application:,,,/Resources/Planes/plane_{0}.png", ((PlaneStateType)value).ToString()), UriKind.Absolute);
+            return new BitmapImage(url);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
